50399bf478023fc7ed8e323d3232b6572eb27907
asalmohammadii <148355208+asalmohammadii@users.noreply.github.com>
2024-11-06 07:52:34 -0800

    Inheritance

---
+++ WindowsFormsApp12/WindowsFormsApp12/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp12
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public class Vehicle
+        {
+            public virtual void Drive() { }
+        }
+        public  class Car:Vehicle
+        {
+            public override void Drive() { }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Car car = new Car();
+            car.Drive();
+
+        }
+    }
+}
