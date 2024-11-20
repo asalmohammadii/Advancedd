27a29344b42197db5a2b99ba4b5b6149c19e392f
asalmohammadii <148355208+asalmohammadii@users.noreply.github.com>
2024-11-20 07:02:52 -0800

    Interface , abstract

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    interface Iloperations
+    {
+        void Add();
+        void Subtract();
+    }
+    class Calculator : Iloperations
+    {
+        public void Add()
+        {
+
+        }
+        public void Subtract()
+        {
+
+        }
+    }
+    abstract class Vehicle
+    {
+       public abstract void Move();
+    }
+    class Car:Vehicle
+    {
+        public override void Move()
+        {
+        }
+    }
+    class Bicycle : Vehicle
+    {
+        public override void Move()
+        {
+        }
+    }
+
+}
+
